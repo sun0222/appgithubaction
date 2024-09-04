f8f3ec7827d89122ab60993c2206a3f9a86728f7
Sunil Kumar <sunil.sunil@digimantra.com>
2024-09-04 13:26:11 +0530

    change in the test cases

--- test/test_operation.py
+++ test/test_operation.py
@@ -2,6 +2,10 @@ from src.math_operations import *
 
 def test_add():
     assert add(2,5)==7
-    assert sub(2,5)==-3
-    assert mul(2,5)==10
-    assert div(6,2)==3
+    assert add(2,8)==10
+
+
+def test_sub():
+    assert add(2,5)==-3
+    assert add(2,8)==-6
+   
